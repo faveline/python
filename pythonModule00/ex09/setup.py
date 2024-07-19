2ccce7091647a4b066351ca4c5c7b8bc5a2922a5
faveline <florian.aveline@outlook.fr>
2024-07-19 11:56:55 +0200

    python mod00 ended

--- pythonModule00/ex09/setup.py
+++ pythonModule00/ex09/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 VERSION = '0.0.1'
 DESCRI = "A sample test package"
@@ -6,18 +6,18 @@ HP = "https://github.com/faveline/exemple"
 LIC = "MIT"
 
 setup(
-        name="ft_package", 
-        version=VERSION,
-		Summary=DESCRI,
-		Home_page=HP,
-        author="faveline",
-        author_email="<faveline@42mulhouse.com>",
-		License=LIC,
-		Location="/home/faveline/python/pythonModule00/ex09/ft_package",
-		Requires="",
-		Required_by="",
-		Metadata_Version="2.1",
-		Installer="pip",
-		Classifiers="",
-		Entry_points=""
+    name="ft_package",
+    version=VERSION,
+    Summary=DESCRI,
+    Home_page=HP,
+    author="faveline",
+    author_email="<faveline@42mulhouse.com>",
+    License=LIC,
+    Location="/home/faveline/python/pythonModule00/ex09/ft_package",
+    Requires="",
+    Required_by="",
+    Metadata_Version="2.1",
+    Installer="pip",
+    Classifiers="",
+    Entry_points=""
 )
