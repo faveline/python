9fd2dc0caddaf6a834f8919c1db1b3e3ceb4c1c6
faveline <florian.aveline@outlook.fr>
2024-07-24 16:47:59 +0200

    python02 ex02 ok

--- pythonModule02/ex02/aff_pop.py
+++ pythonModule02/ex02/aff_pop.py
@@ -1,23 +1,56 @@
 from load_csv import load
 from matplotlib import pyplot as plt
 import numpy as np
+import sys
+
+
+def ft_yaxis(x: int):
+    if (x / 1000000000 >= 1):
+        x = x / 1000000000
+        return ("{:.2f}G".format(x))
+    if (x / 1000000 >= 1):
+        x = x / 1000000
+        return ("{:.0f}M".format(x))
+    if (x / 1000 >= 1):
+        x = x / 1000
+        return ("{:.0f}k".format(x))
 
 
 def main():
-	try:
-		data = load("../life_expectancy_years.csv")
-	except Exception as msg:
-		print(msg)
-		return
+    try:
+        assert len(sys.argv) == 2, "AssertionError: the arguments are bad"
+    except AssertionError as msg:
+        print(msg)
+        return
+    try:
+        data = load("../population_total.csv")
+        found = data.set_index("country").transpose()[sys.argv[1]]
+    except Exception as msg:
+        print("ExceptionError:", msg)
+        return
 
-	data = data.set_index("country").transpose()
+    cpy = data.set_index("country").transpose()
+    dataFr = cpy["France"]
+    limx = np.where(np.array(cpy.index) == "2050")[0][0]
+    X = np.array(cpy.index)[:limx]
+    Yfr = np.array(dataFr.replace({'B': 'e+09', 'M': 'e+06', 'k': 'e+03'},
+                                  regex=True))[:limx].astype(float).astype(int)
+    Yarg = np.array(found.replace({'B': 'e+09', 'M': 'e+06', 'k': 'e+03'},
+                                  regex=True))[:limx].astype(float).astype(int)
 
-	plt.plot(np.array(data.index), np.array(data["France"]))
-	plt.title("France Life Expectancy Projections")
-	plt.xlabel("Years")
-	plt.ylabel("Life Expectancy")
-	plt.xticks([a for a in np.array(data.index) if int(a) / 40 == int(int(a) / 40)])
-	plt.show()
+    plt.plot(X, Yarg, color='b', label=sys.argv[1])
+    plt.plot(X, Yfr, color='g', label="France")
+    plt.title("Population Projections")
+    plt.xlabel("Year")
+    plt.ylabel("Population")
+    plt.xticks([a for a in np.array(cpy.index)[:limx] if
+                int(a) / 40 == int(int(a) / 40)])
+    plt.yticks(np.arange(0, max(Yfr[-1], Yarg[-1]) * 1.05, 20000000))
+    current_values = plt.gca().get_yticks()
+    plt.gca().set_yticklabels([ft_yaxis(x) for x in current_values])
+    plt.legend()
+    plt.show()
+
 
 if __name__ == "__main__":
-	main()
+    main()
